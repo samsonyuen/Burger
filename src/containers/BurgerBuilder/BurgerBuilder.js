073b9b369dc9c00e17d27347a05f76c27046cbc7
Samson Yuen <samson.yuen@outlook.com>
2019-02-03 20:17:24 -0500

    setup basic structure and add burger ingredients

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class BurgerBuilder extends Component {
+  render () {
+    return (
+        <>
+          <div>Burger Preview</div>
+          <div>Build Controls</div>
+        </>
+    );
+  }
+}
+
+export default BurgerBuilder;
