3d86ceb49944a17b6750c7a975519303ae80968f
Samson Yuen <samson.yuen@outlook.com>
2019-02-03 21:32:55 -0500

    add state to burger builder and dynamically build burger based on state

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+      .map(ing => {
+        return [...Array(props.ingredients[ing])].map(
+            (_, index) => {
+              return <BurgerIngredient key= {ing + index} type={ing}/>
+            }
+        );
+      });
+
   return (
         <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"/>
-          <BurgerIngredient type="cheese"/>
-          <BurgerIngredient type="meat"/>
+          {transformedIngredients}
           <BurgerIngredient type="bread-bottom"/>
         </div>
   );
