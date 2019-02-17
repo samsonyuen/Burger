1f3102aa8f232f225eaf70148c13b7e21462ff17
Samson Yuen <samson.yuen@outlook.com>
2019-02-16 20:08:59 -0500

    add navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => {
   return (
       <>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar>Toolbar, SideDrawer, Backdrop</Toolbar>
         <main className={classes.Content}>
           {props.children}
         </main>
